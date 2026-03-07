c976589b1031c99faae226b96429dd9588996276
aurelabode <lelabode@hotmail.com>
2026-03-07 21:52:29 +0100

    struct

--- piscine_practice/arg1.c
+++ piscine_practice/arg1.c
@@ -1,24 +1,25 @@
 # include <stdio.h>
-# include<string.h>
-# include<stdbool.h>
+# include <_string.h>
+# include <stdbool.h>
 
-typedef struct {
-  char name [50];
+typedef struct{
+  char name[50];
   int age;
   double gpa;
   bool isFullTime;
 }Student;
+
 void printStudent(Student student);
 
 int main(){
-  Student student1= {"Tomas",22,3.5,true};
-  Student student2 = {"Vincent",21, 3.4, true};
-  Student student3 = {"Michael",18,4.0,true};
+  Student student1 = {"Maria",24,3.5,true};
+  Student student2 = {"Marta",22,2.1,true};
+  Student student3 = {"Mark", 36, 4.0, false};
   Student student4 = {0};
-  strncpy(student4.name,"Lisa",4);
-  student4.age= 20;
-  student4.gpa = 3.5;
-  student4.isFullTime= false;
+  strncpy( student4.name,"Martin",6);
+  student4.age = 23;
+  student4.gpa = 2.4;
+  student4.isFullTime = false;
   printStudent(student1);
   printStudent(student2);
   printStudent(student3);
@@ -26,9 +27,9 @@ int main(){
   return 0;
 }
 void printStudent(Student student){
-  printf("%s\n",student.name);
-  printf("%d\n",student.age);
-  printf("%.2lf\n",student.gpa);
-  printf("%s\n",(student.isFullTime)? "Yes" :"No");
+  printf("Name: %s\n",student.name);
+  printf("Age: %d\n",student.age);
+  printf("GPA: %.2f\n", student.gpa);
+  printf("Full-Time: %s\n", (student.isFullTime)? "YES" :"NO");
   printf("\n");
 }
